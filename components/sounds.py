4f1f659be5002e3bbe0908e73dc51e27794767ff
Luk Krzy <lukaskrzy@gmail.com>
2020-11-30 15:05:35 +0100

    Fix Add sounds

---
+++ components/sounds.py
@@ -0,0 +1,9 @@
+import simpleaudio as sa
+
+SOUNDS = {
+
+    'sword': sa.WaveObject.from_wave_file('components/sound/sword_attack.wav'),
+    'crit': sa.WaveObject.from_wave_file('components/sound/crit_1.wav'),
+    'die': sa.WaveObject.from_wave_file('components/sound/die.wav')
+
+}
